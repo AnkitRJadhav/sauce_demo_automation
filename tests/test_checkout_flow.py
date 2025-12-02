b06edbce3eda3a2460d1ed315bcabc13f3fbc118
AnkitRJadhav <ankitjadhav9925@gmail.com>
2025-12-02 20:32:35 +0530

    Adding Headless Mode

--- tests/test_checkout_flow.py
+++ tests/test_checkout_flow.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from playwright.sync_api import sync_playwright, Page
 from pages.login_page import LoginPage
@@ -11,7 +10,7 @@ from config.config import config
 @pytest.fixture(scope="function")
 def page() -> Page:
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         yield page
